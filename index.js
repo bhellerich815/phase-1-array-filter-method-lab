90b46569708eec40532e13194bcff9b896d91ac3
Brooke Hellerich <brookeh815@cox.net>
2021-10-21 14:29:16 -0500

    commit tree 844d3a617a880f7fcd75aee45711172866d63a34

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+    let result = drivers.filter(driver => {
+       return driver.toLowerCase() === string.toLowerCase()
+
+    })
+    return result
+}
+
+function fuzzyMatch(drivers, string) {
+    const result = drivers.filter(driver => driver === string.split(`"Sammy", "Sally", "Sarah"`))
+    return result
+}
+
+function fuzzyMatch(drivers, string) {
+    let result = drivers.filter(driver => {
+    // debugger
+    return driver.indexOf(string) === 0
+    })
+    return result
+}
